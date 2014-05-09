89bc1a208000c6e846f41dc6201bc8df96813d4b
Apprentice <apprentice@dbc-chicago-008.local>
2014-05-09 11:40:33 -0500

    more current branch

--- source/app/helpers/helpers.rb
+++ source/app/helpers/helpers.rb
@@ -7,7 +7,7 @@ helpers do
   def get_loser_posts(cat)
     length = Post.sort_by_votes(cat).length
     if length < 10
-      return Post.sort_by_votes(cat)
+      return []
     else
       return Post.sort_by_votes(cat).slice(10..length)
     end
--- source/public/js/toggle_posts.js
+++ source/public/js/toggle_posts.js
@@ -1,28 +1,60 @@
 $(function() {
-  $('[class^="all_resource"]').hide();
+  // $('[class^="all_resource"]').hide();
 
-  $('[id= "img0"]').on("click", function() {
-    var loserPosts = $('.all_resource0').html()
-    $('.resource_col0').append(loserPosts);
-  });
+  // $('[id= "img0"]').on("click", function() {
+  //   var loserPosts = $('.all_resource0').html()
+  //   $('.resource_col0').append(loserPosts);
+  // });
 
-  $('[id= "img1"]').on("click", function() {
-    $('.resource_col1, .all_resource1').toggle(5);
-  });
+  // $('[id= "img1"]').on("click", function() {
+  //   $('.resource_col1, .all_resource1').toggle(5);
+  // });
 
-  $('[id= "img2"]').on("click", function() {
-    $('.resource_col2, .all_resource2').toggle(5);
-  });
+  // $('[id= "img2"]').on("click", function() {
+  //   $('.resource_col2, .all_resource2').toggle(5);
+  // });
 
-  $('[id= "img3"]').on("click", function() {
-    $('.resource_col3, .all_resource3').toggle(5);
-  });
+  // $('[id= "img3"]').on("click", function() {
+  //   $('.resource_col3, .all_resource3').toggle(5);
+  // });
 
-  $('[id= "img4"]').on("click", function() {
-    $('.resource_col4, .all_resource4').toggle(5);
-  });
+  // $('[id= "img4"]').on("click", function() {
+  //   $('.resource_col4, .all_resource4').toggle(5);
+  // });
 
-  $('[id= "img5"]').on("click", function() {
-    $('.resource_col5, .all_resource5').toggle(5);
-  });
+  // $('[id= "img5"]').on("click", function() {
+  //   $('.resource_col5, .all_resource5').toggle(5);
+  // });
+
+  // $(".show_loser").detach();
+  // $(".show_loser").hide();
+
+  $('.image').on("click", function(){
+    console.log("oh hai");
+    var showLoser = $(this).parent().children(".show_loser")
+    // console.log(showLoser);
+    // showLoser.toggleClass()
+    // console.log(showLoser.css());
+    // showLoser.show();
+    // console.log($(this).parent().children(".show_loser").show().;
+  showLoser.toggle(function () {
+    $("showLoser").css({display: "none"});
+}, function () {
+    $("showLoser").css({display: "inline-block"});
 });
+
+  })
+
+
+
+
+
+  // $("#img1").on("click", function(){
+  //   console.log("clicking on img 1");
+  //   $(".resrouce_col").show(".all_resource");
+  // })
+
+
+});
+
+
