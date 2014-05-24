8511b3585833a2eab77b0fc4466bd8a875769fa3
Justin Stewart <stewart.justin.m@gmail.com>
2014-05-24 15:47:16 -0500

    style: replaced headline in posts_by_tag.erb with _nav.erb partial

--- source/app/controllers/tags.rb
+++ source/app/controllers/tags.rb
@@ -17,6 +17,7 @@ end
 get '/tags/:id' do
   @tag = Tag.find(params[:id])
   @tag_name = @tag.name.gsub(',', '').upcase
+  @header = "All Posts with #{@tag_name}"
   @posts = @tag.posts
   erb :'posts/posts_by_tag'
 end
