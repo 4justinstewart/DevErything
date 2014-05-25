6cce3d188506db21b699ebc6fbda8a8c0c7c4124
Justin Stewart <stewart.justin.m@gmail.com>
2014-05-25 12:37:20 -0500

    chore: changed toggle_login.js file name to authenticate.js

--- source/public/js/authenticate.js
+++ source/public/js/authenticate.js
@@ -1,11 +1,6 @@
 $(function() {
   $('.auth_button').on('click', function(){
-    // $('#signin').hide();
-    // $('#signup').show();
-    // $(this).html('Sign In');
-
     $('.authenticate').toggle(600);
-    console.log(this);
     $('.auth_button').toggle();
   });
 });
